95ec53d946a22ef446318908aa0c6e61f94b3c13
omar <omaraser93@gmail.com>
2020-10-17 16:58:33 +0200

    Updates

---
+++ precache-manifest.27124240b5f5703dc69be698b8069ac1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97b084461ad5c8bd02df094d2b385c09",
+    "url": "/Recipes-App/index.html"
+  },
+  {
+    "revision": "a8a60fea9bcf450e99c0",
+    "url": "/Recipes-App/static/css/main.e72dcf3b.chunk.css"
+  },
+  {
+    "revision": "62eecd07bab9f7c97932",
+    "url": "/Recipes-App/static/js/2.5689930d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipes-App/static/js/2.5689930d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8a60fea9bcf450e99c0",
+    "url": "/Recipes-App/static/js/main.61fcc23a.chunk.js"
+  },
+  {
+    "revision": "38f6df42fb79632147a0",
+    "url": "/Recipes-App/static/js/runtime-main.e993f1cd.js"
+  }
+]);
